817d8869fa6a81a302559fdf3fbd565dcd4f3f59
CrazyGIX <crazygix@yahoo.com>
2016-05-24 16:02:14 +0200

    Establishing connection with server v2.0

--- RemoteNiP-client/remoteclient.cpp
+++ RemoteNiP-client/remoteclient.cpp
@@ -3,12 +3,178 @@
 
 RemoteClient::RemoteClient(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::RemoteClient)
-{
+    ui(new Ui::RemoteClient){
     ui->setupUi(this);
+
+    m_emptyString = "";
+
+    tcpSocket = NULL;
+
+    tcpServer = new QTcpServer(this);
+    udpSocket = new QUdpSocket(this);
+    broadcastTimer =  new QTimer(this);
+    connectionRequestTimer = new QTimer(this);
+
+    connect(tcpServer, SIGNAL(newConnection()), this, SLOT(newConnection()));
+    connect(broadcastTimer, SIGNAL(timeout()), this, SLOT(sendBroadcast()));
+    connect(connectionRequestTimer, SIGNAL(timeout()), this, SLOT(sendConnectionRequest()));
+
 }
 
-RemoteClient::~RemoteClient()
-{
+
+
+
+
+RemoteClient::~RemoteClient(){
     delete ui;
 }
+
+
+void RemoteClient::on_searchButton_clicked(){
+    initialize();
+    sendBroadcast();
+    broadcastTimer->start(5000);
+
+    emit broadcastingStarted();
+}
+
+void RemoteClient::initialize(){
+    tcpServer->close();
+    tcpServer->listen(QHostAddress::LocalHost, m_port);
+
+
+
+    udpSocket->close();
+    connect(udpSocket, SIGNAL(readyRead()),this,SLOT(incomingUdpData()));
+    udpSocket->bind(m_port,QUdpSocket::DontShareAddress | QUdpSocket::ReuseAddressHint);
+}
+
+void RemoteClient::sendBroadcast(){
+    QByteArray datagram = "NiP:Broadcast";
+
+    udpSocket->writeDatagram(datagram, QHostAddress::LocalHost, m_port);
+}
+
+void RemoteClient::sendConnectionRequest(){
+    QByteArray datagram = "NiP:Hello";
+
+    udpSocket->writeDatagram(datagram, m_hostAddress, m_port);
+}
+
+void RemoteClient::newConnection(){
+    if (!tcpSocket) {
+        tcpSocket = tcpServer->nextPendingConnection();
+
+        connect(tcpSocket, SIGNAL(disconnected()), this, SLOT(deleteConnection()));
+        connect(tcpSocket, SIGNAL(readyRead()), this, SLOT(incomingData()));
+
+        emit connected();
+        abortConnectionRequest();
+
+        networkTimeoutTimer->start();
+    }
+}
+
+void RemoteClient::incomingUdpData(){
+    QHostAddress hostAddress;
+    QByteArray datagram;
+    datagram.resize(udpSocket->pendingDatagramSize());
+
+    udpSocket->readDatagram(datagram.data(), datagram.size(), &hostAddress);
+
+    const QString magicMessageConnected = "NiP:Connected";
+    const QString magicMessageNotConnected = "NiP:NotConnected";
+    const QString magicMessageServerConnecting = "NiP:Connecting";
+
+    if (datagram == magicMessageConnected){
+        addServer(hostAddress, true);
+    }
+    else if (datagram == magicMessageNotConnected){
+        addServer(hostAddress, false);
+    }
+    else if (datagram == magicMessageServerConnecting){
+        emit serverConnecting();
+    }
+}
+
+void RemoteClient::abortConnectionRequest(){
+    connectionRequestTimer->stop();
+}
+
+void RemoteClient::addServer(QHostAddress hostAddress, bool connected){
+    bool found = false;
+    for (int i = 0; i < serverList.size(); i++){
+        if (serverList.at(i).hostAddress == hostAddress) {
+            found = true;
+            serverList[i].connected = connected;
+            break;
+        }
+    }
+    if (!found){
+        QRCServer qrcServer;
+        qrcServer.hostAddress = hostAddress;
+        qrcServer.hostName = hostAddress.toString();
+        qrcServer.connected = connected;
+        serverList.append(qrcServer);
+
+        QHostInfo::lookupHost(hostAddress.toString(),
+                               this, SLOT(saveResolvedHostName(QHostInfo)));
+    }
+
+    emit serversCleared();
+    for (int i = 0; i < serverList.size(); i++){
+        emit serverFound(serverList.at(i).hostAddress.toString(),
+                         serverList.at(i).hostName,
+                         serverList.at(i).connected);
+    }
+}
+
+void RemoteClient::clearServerList(){
+    serverList.clear();
+    emit serversCleared();
+}
+
+
+void RemoteClient::incomingData(){
+    QByteArray data = tcpSocket->readAll();
+   // int type = data.left(1).toInt();
+
+    int payloadsize = data.mid(1,10).toInt();   //getting the size of the whole payload
+                    data = data.mid(11);                        //removing obsolete data
+                    while (data.size() != payloadsize) {        //wait for the missing data
+                        tcpSocket->waitForReadyRead();
+                        data.append(tcpSocket->readAll());
+                    }
+
+/*    switch (type) {
+        case 1: incomingIcon(data);                         //process the data
+                break;
+        case 2: incomingAmarokData(data);
+                break;
+    }
+*/
+}
+
+void RemoteClient::deleteConnection(){
+    tcpSocket->abort();
+    tcpSocket->deleteLater();
+    tcpSocket = NULL;
+
+    networkTimeoutTimer->stop();
+
+    emit disconnected();
+}
+
+void RemoteClient::updateNetConfig(){
+    if ((session == NULL) || (!session->isOpen()))
+    {
+        netConfigManager->updateConfigurations();
+    }
+}
+
+
+void RemoteClient::abortBroadcasting(){
+    broadcastTimer->stop();
+}
+
+
--- RemoteNiP-client/remoteclient.h
+++ RemoteNiP-client/remoteclient.h
@@ -2,6 +2,9 @@
 #define REMOTECLIENT_H
 
 #include <QMainWindow>
+#include <QWidget>
+#include <QString>
+#include <QtNetwork>
 
 namespace Ui {
 class RemoteClient;
@@ -15,8 +18,128 @@ public:
     explicit RemoteClient(QWidget *parent = 0);
     ~RemoteClient();
 
+    typedef struct{
+        QHostAddress hostAddress;
+        QString hostName;
+        bool connected;
+    }QRCServer;
+
+public slots:
+    void abortBroadcasting();
+    void abortConnectionRequest();
+    void clearServerList();
+
+    QString hostName() const{
+        return m_hostname;
+    }
+
+    int port() const{
+        return m_port;
+    }
+
+    QHostAddress hostAddress() const{
+        return m_hostAddress;
+    }
+
+    int networkTimeout() const{
+        return m_networkTimeout;
+    }
+
+    QString getEmptyString() const{
+        return m_emptyString;
+    }
+
+public slots:
+    void setHostname(QString arg)
+    {
+        if (m_hostname != arg) {
+            m_hostname = arg;
+            emit hostnameChanged(arg);
+        }
+
+        QHostAddress hostAddress;
+        if (!m_hostname.isEmpty())
+            hostAddress.setAddress(m_hostname);
+        else
+            hostAddress = QHostAddress::Broadcast;
+
+        setHostAddress(hostAddress);
+    }
+
+    void setHostAddress(QHostAddress arg)
+    {
+        if (m_hostAddress != arg) {
+            m_hostAddress = arg;
+
+            m_hostname = m_hostAddress.toString();
+            emit hostAddressChanged(arg);
+            emit hostnameChanged(m_hostname);
+        }
+    }
+
+    void setPort(int arg)
+    {
+        if (m_port != arg) {
+            m_port = arg;
+            emit portChanged(arg);
+        }
+    }
+
+    void setNetworkTimeout(int arg)
+    {
+        if (m_networkTimeout != arg) {
+            m_networkTimeout = arg;
+            emit networkTimeoutChanged(arg);
+        }
+    }
+
+signals:
+    void hostnameChanged(QString arg);
+    void hostAddressChanged(QHostAddress arg);
+    void portChanged(int arg);
+    void connected();
+    void disconnected();
+    void broadcastingStarted();
+    void connectingStarted();
+    void serverConnecting();
+    void networkTimeoutChanged(int arg);
+    void emptyStringChanged(QString arg);
+    void serversCleared();
+    void serverFound(QString address, QString hostName, bool connected);
+
+
 private:
     Ui::RemoteClient *ui;
+    QUdpSocket *udpSocket;
+    QTcpSocket *tcpSocket;
+    QTcpServer *tcpServer;
+    QNetworkSession *session;
+    QNetworkConfigurationManager *netConfigManager;
+    QTimer     *netConfigTimer;
+    QTimer     *networkTimeoutTimer;
+    QTimer     *broadcastTimer;
+    QTimer     *connectionRequestTimer;
+
+    QList<QRCServer> serverList;
+
+    void addServer(QHostAddress hostAddress,bool connected);
+
+    QString         m_hostname;
+    int             m_port;
+    QHostAddress    m_hostAddress;
+    int             m_networkTimeout;
+    QString         m_emptyString;
+
+private slots:
+    void sendConnectionRequest();
+    void sendBroadcast();
+    void newConnection();
+    void deleteConnection();
+    void incomingData();
+    void incomingUdpData();
+    void updateNetConfig();
+    void initialize();
+    void on_searchButton_clicked();
 };
 
 #endif // REMOTECLIENT_H
